1a427bce91a1afeedb382ed559b09d0affe98e56
Ali Muhammadov <alimahammadov@users.noreply.github.com>
2026-09-04 02:26:54 +0400

    fix(security): update revoked Telegram Bot token to kill ghost instances and bind new token

--- Application/DTOs/AppConfig.cs
+++ Application/DTOs/AppConfig.cs
@@ -7,7 +7,7 @@ namespace CryptoSense.Application.DTOs
         public string SuperAdminTelegram { get; set; } = "@Ali_Mahammadov";
         public string SuperAdminChatId { get; set; } = "1219998176";
         public long SuperAdminUserId { get; set; } = 1219998176;
-        public string TelegramBotToken { get; set; } = "8671151605:AAHHPojQbGiSKtJUkeQQCnITfTHInQ3tX5U";
+        public string TelegramBotToken { get; set; } = "8671151605:AAG57j5r_Uwwq7o_9rlUnjGkyTp5d5BZHWI";
         public bool AutoScanEnabled { get; set; } = true;
         public int MinConfidenceThreshold { get; set; } = 78;
         public List<string> SelectedCoins { get; set; } = new();
